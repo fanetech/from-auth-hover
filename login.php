4af38e15960e1ce063bb495d15c5184ce247fe87
FIRST_NAME LAST_NAME <louguefranck.20@gmail.com>
2022-06-28 15:54:17 +0000

    add php code of connect user and connect to database

--- login.php
+++ login.php
@@ -19,43 +19,66 @@
 
 
             <?php
-            if(isset($_POST["valider"])){
+            if (isset($_POST["valider"])) {
 
-            
-            if (isset($_POST["login"]) && isset($_POST["password"])) {
-                $login = strtolower($_POST["login"]);
-                $password = $_POST["password"];
 
-                if ($login == "fanetech" && $password == "man") {
-                    echo "<h1>Connexion reussi</h1>";
+                if (isset($_POST["login"]) && isset($_POST["password"])) {
+                    $login = strtolower($_POST["login"]);
+                    $password = $_POST["password"];
+                    $db = mysqli_connect('localhost', 'root', '', 'ifoad');
+                    if ($db->connect_errno != 0) {
+                        echo "Erreur de connexion a la base de données";
+                    } else {
+                        $nom = $_POST['login'];
+                        $prenom = $_POST['passwd'];
+                        $req = "INSERT INTO information (nom, prenom) VALUES ('$nom', '$prenom')";
+                        $result = mysqli_query($db, $req);
+                        $nb = mysqli_num_rows($result);
+                        if ($nb) {
+                            session_start();
+                            $_SESSION['login'] = $login;
+                            $result = "Information ajouté avec succès";
+                            $typeMessage = "success";
+                        } else {
+                            $result = "information incorrect";
+                            $typeMessage = "error";
+                        }
+                    }
                 } else {
-                    echo "<h2>identifiant incorrect</h2>";
+                    $result = "Erreur: Login ou mot de passe incorrectes !";
+                    $typeMessage = "error";
                 }
             } else {
                 echo "<h2>Renseigner tous les champs</h2>";
             }
-        }
+
+
+            ?>
+        </div>
+        <div>
+            <?php
+            echo $result;
 
             ?>
         </div>
         <form action="login.php" method="post">
             <div class="txt-field">
                 <input type="text" name="login" required>
-                <label>Username</label>
+                <label>nom</label>
                 <span></span>
             </div>
             <div class="txt-field">
                 <input type="password" name="password" required>
-                <label>Password</label>
+                <label>Prenom</label>
                 <span></span>
             </div>
-            <div class="pass">
+            <!-- <div class="pass">
                 Forgot password
-            </div>
+            </div> -->
             <input type="submit" name="valider" value="Login">
-            <div class="signup_link">
+            <!-- <div class="signup_link">
                 Nom a member? <a href="#">SignIn</a>
-            </div>
+            </div> -->
 
         </form>
     </div>
